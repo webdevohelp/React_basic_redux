c797eaa30b6dddaea33d042b72b71c790fb643db
Web Dev Help <43485053+webdevohelp@users.noreply.github.com>
2021-12-13 19:17:52 +0530

    completed the basic redux project
    completed the basic redux project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+
+import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const isAuth = useSelector((state) => state.auth.isAuthenticated);
+    return (
+        <Fragment>
+            <Header />
+            {!isAuth && <Auth />}
+            {isAuth && <UserProfile />}
+            <Counter />
+        </Fragment>
+    );
 }
 
 export default App;
